71e77fe6c33ec8ef5f1797e5602c623f2a65a9a3
Greg Schoberth <gschober@asu.edu>
2020-03-29 14:36:58 -0400

    Initial Setup

---
+++ client/build-utils/webpack.common.js
@@ -0,0 +1,63 @@
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.(css)$/i,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          },
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Webpack App",
+      template: "./src/index.html"
+    })
+  ],
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+  }
+};
---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./sass/style.scss";
+
+const App = () => {
+  return <h1>This is a</h1>;
+};
+
+export default App;
